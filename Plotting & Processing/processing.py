f695af8e948df40a416275baddcfb92313fd59de
dennisblaufuss <dennis_blaufuss@web.de>
2021-11-06 14:01:04 +0100

    shit

--- Plotting & Processing/processing.py
+++ Plotting & Processing/processing.py
@@ -1,20 +1,44 @@
 import pandas as pd
+import numpy as np
 import io
 import requests
 import torch
 
+
+device = 'cpu'
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# You may want to turn this one on if you don't have a mac
+
 url_lnd = "https://raw.githubusercontent.com/SophieMerl/DataAnaytics_London/master/data/London_301021.csv"
 download_lnd = requests.get(url_lnd).content
 df_lnd = pd.read_csv(io.StringIO(download_lnd.decode('utf-8')))
-print(df_lnd.head())
 
 url_cvd = "https://raw.githubusercontent.com/SophieMerl/DataAnaytics_London/master/data/London_Covid_301021.csv"
-download_cvdurl_cvd = requests.get(url_cvd).content
-df_cvdurl_cvd = pd.read_csv(io.StringIO(download_cvdurl_cvd.decode('utf-8')))
-print(df_cvdurl_cvd.head())
+download_cvd = requests.get(url_cvd).content
+df_cvd = pd.read_csv(io.StringIO(download_cvd.decode('utf-8')))
+
+x_train = torch.from_numpy(df_lnd[1][:len(df_lnd[]) * 0.8]).float().to(device)
+x_test = torch.from_numpy().float().to(device)
+
+y_train = torch.from_numpy().float().to(device)
+y_test = torch.from_numpy().float().to(device)
 
 
-x = torch.rand(5, 3)
-print(x)
+lr = 1.3e-1
+n_epochs = 1000
 
+a = torch.randn(1, dtype=torch.float).to(device)
+b = torch.randn(1, dtype=torch.float).to(device)
+a.requires_grad_()
+b.requires_grad_()
 
+for epoch in range(n_epochs):
+    yhat = a + b * x_train
+    error = y_train - yhat
+    loss = (error ** 2).mean()
+    loss.backward()
+    with torch.no_grad():
+        a -= lr * a.grad
+        b -= lr * b.grad
+    a.grad.zero_()
+    b.grad.zero_()
