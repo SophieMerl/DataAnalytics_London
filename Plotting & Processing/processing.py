f246d917329896012c39d446b0060c211cd8b099
dennisblaufuss <dennis_blaufuss@web.de>
2021-11-06 09:48:50 +0100

    receiving data

--- Plotting & Processing/processing.py
+++ Plotting & Processing/processing.py
@@ -1,4 +1,18 @@
+import pandas as pd
+import io
+import requests
 import torch
+
+url_lnd = "https://raw.githubusercontent.com/SophieMerl/DataAnaytics_London/master/data/London_301021.csv"
+download_lnd = requests.get(url_lnd).content
+df_lnd = pd.read_csv(io.StringIO(download_lnd.decode('utf-8')))
+print(df_lnd.head())
+
+url_cvd = "https://raw.githubusercontent.com/SophieMerl/DataAnaytics_London/master/data/London_Covid_301021.csv"
+download_cvdurl_cvd = requests.get(url_cvd).content
+df_cvdurl_cvd = pd.read_csv(io.StringIO(download_cvdurl_cvd.decode('utf-8')))
+print(df_cvdurl_cvd.head())
+
 
 x = torch.rand(5, 3)
 print(x)
