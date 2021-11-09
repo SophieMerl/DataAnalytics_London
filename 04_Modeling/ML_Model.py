f54e00aef6d72a928d355354e65bc628bcb3d708
dennisblaufuss <dennis_blaufuss@web.de>
2021-11-10 00:01:38 +0100

    date fix

--- 04_Modeling/ML_Model.py
+++ 04_Modeling/ML_Model.py
@@ -18,12 +18,14 @@ device = 'cpu'
 url_lnd = "https://raw.githubusercontent.com/SophieMerl/DataAnaytics_London/master/02_Preprocessing/London_cleaned_unpivoted.csv"
 download_lnd = requests.get(url_lnd).content
 df_lnd = pd.read_csv(io.StringIO(download_lnd.decode('utf-8')))
+df_lnd["Date"] = pd.to_datetime(df_lnd["Date"])
 df_lnd_grouped = df_lnd.groupby('Date').mean()
 
 # receiving covid data, formating date & computing the rolling sum over last 14 days
 url_cvd = "https://raw.githubusercontent.com/SophieMerl/DataAnaytics_London/master/02_Preprocessing/Covid_cleaned.csv"
 download_cvd = requests.get(url_cvd).content
 df_cvd = pd.read_csv(io.StringIO(download_cvd.decode('utf-8')))
+df_cvd["Date"] = pd.to_datetime(df_cvd["Date"])
 df_cvd["newDeaths28DaysByDeathDate"] = df_cvd["newDeaths28DaysByDeathDate"].rolling(min_periods=1, window=14).sum()
 
 # Training Data is 80% of the data
